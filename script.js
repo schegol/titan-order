202dcfd31baa945a8821594669d92db87cabaf2a
schegol <48925789+schegol@users.noreply.github.com>
2020-01-31 20:07:31 +0300

    init

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  let legalEntity = $('#orderLegalEntity');
+  let companyInfoBlock = $('.company-info');
+  let companyInfo = $('.company-info');
+  let companyFields = $('[class$="--company"]');
+  let personDelivery = $('.payment-delivery--person');
+  let personFields = $('[class$="--person"]');
+  let deliveryCheckbox = $('.payment-delivery__radio');
+  let deliveryCheckboxLabel = $('.payment-delivery__radio-label');
+
+  personFields.show();
+  companyFields.hide();
+  companyInfo.hide();
+
+  legalEntity.on('change', function() {
+    if (this.value == 'person') {
+      personFields.show();
+      personDelivery.show();
+      companyFields.hide();
+      companyInfo.hide();
+    } else {
+      personFields.hide();
+      personDelivery.hide();
+      companyFields.show();
+      companyInfo.show();
+    };
+  });
+
+  deliveryCheckbox.each(function() {
+    if ($(this).prop('checked')) {
+      $('label[for="'+$(this).attr('id')+'"]').addClass('active');
+    };
+  });
+
+  deliveryCheckbox.click(function() {
+    $('input[name="'+$(this).attr('name')+'"]').each(function() {
+      $('label[for="'+$(this).attr('id')+'"]').removeClass('active');
+    });
+    $('label[for="'+$(this).attr('id')+'"]').addClass('active');
+  });
+});
